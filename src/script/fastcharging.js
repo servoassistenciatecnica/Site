be127314455bf9a5a797a222069a75981180b91f
Servoenterprise <servoassistenciatecnica@gmail.com>
2022-12-14 23:07:55 -0300

    site

--- src/script/fastcharging.js
+++ src/script/fastcharging.js
@@ -1,13 +1,101 @@
-function openpag(a){
-    let displaypag = document.querySelector('.displayed')
-    let pag =  new XMLHttpRequest()
+    let home = document.getElementById("home")
+    let loja = document.getElementById("loja")
+    let serviços = document.getElementById("serviços")
+    let treinamentos = document.getElementById("treinamentos")
+    let contatos= document.getElementById("contatos")      
 
-    pag.onreadystatechange = () =>{
-        if (pag.readyState == 4 && pag.status == 200) {
-            displaypag.innerHTML = pag.response
+    function openhome(){
+        if (loja.style.display == "none") {
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "none"
+            contatos.style.display = "none"
+            loja.style.display = "none"
+        }else{
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "none"
+            contatos.style.display = "none"
+            loja.style.display = "none"
         }
     }
 
-    pag.open('GET', `../pages/${a}.html`,)
-    pag.send()
-}
+    function openloja(){
+        if (loja.style.display == "none") {
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "none"
+            contatos.style.display = "none"
+            loja.style.display = "flex"
+        }else{
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "none"
+            contatos.style.display = "none"
+            loja.style.display = "flex"
+        }
+    }
+
+    function openserviços(){
+        if (loja.style.display == "none") {
+            home.style.display = "none"
+            serviços.style.display = "block"
+            treinamentos.style.display = "none"
+            contatos.style.display = "none"
+            loja.style.display = "none"
+        }else{
+            home.style.display = "none"
+            serviços.style.display = "block"
+            treinamentos.style.display = "none"
+            contatos.style.display = "none"
+            loja.style.display = "none"
+        }
+
+    }
+
+    function opentreinamentos(params) {
+        if (loja.style.display == "none") {
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "block"
+            contatos.style.display = "none"
+            loja.style.display = "none"
+        }else{
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "block"
+            contatos.style.display = "none"
+            loja.style.display = "none"
+        }
+    }
+
+    function opencontatos() {
+        if (loja.style.display == "none") {
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "none"
+            contatos.style.display = "block"
+            loja.style.display = "none"
+        }else{
+            home.style.display = "none"
+            serviços.style.display = "none"
+            treinamentos.style.display = "none"
+            contatos.style.display = "block"
+            loja.style.display = "none"
+        }
+    }
+
+
+
+
+
+
+
+
+/*
+    if (cont.style.display == "none") {
+        cont.style.display = "block"
+    }else{
+        cont.style.display = "block"
+    }
+    */
