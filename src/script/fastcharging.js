6f906a393d5922378937cc3afdbbe5279af3e8fa
Servoenterprise <servoassistenciatecnica@gmail.com>
2022-12-16 07:35:52 -0300

    site

--- src/script/fastcharging.js
+++ src/script/fastcharging.js
@@ -1,8 +1,9 @@
     let home = document.getElementById("home")
-    let loja = document.getElementById("loja")
+    let loja = document.getElementById("buttosviewloja")
     let serviços = document.getElementById("serviços")
     let treinamentos = document.getElementById("treinamentos")
-    let contatos= document.getElementById("contatos")      
+    let contatos= document.getElementById("contatos")   
+    let viloja = document.getElementById("viewloja")     
 
     function openhome(){
         if (loja.style.display == "none") {
@@ -22,17 +23,24 @@
 
     function openloja(){
         if (loja.style.display == "none") {
+            viloja.style.display = "flex"
             home.style.display = "none"
             serviços.style.display = "none"
             treinamentos.style.display = "none"
             contatos.style.display = "none"
             loja.style.display = "flex"
+            loja.style.width = "100vw"
+            viloja.style.width = "100vw"
         }else{
             home.style.display = "none"
             serviços.style.display = "none"
             treinamentos.style.display = "none"
             contatos.style.display = "none"
-            loja.style.display = "flex"
+            loja.style.display = "none"
+            loja.style.width = "0vh"
+            viloja.style.width = "0vh"
+            viloja.style.display = "none"
+            
         }
     }
 
