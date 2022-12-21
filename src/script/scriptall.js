72a5efd4329582e428d507cc2ef309a99908ef7d
servoassistenciatecnica <servoassistenciatecnica@gmail.com>
2022-12-21 16:27:03 -0300

    site

--- src/script/scriptall.js
+++ src/script/scriptall.js
@@ -1,7 +1,7 @@
-function loading_p () {
+function loading_p() {
     document.getElementById('box-load').style.display = "none";
     document.getElementById('afterheader').style.display = "block";
     document.getElementById('beforeheader').style.display = "flex";
-    document.getElementById('container').style.display = "block";
+    document.getElementById('berofeslider').style.display = "block";
 }
 
