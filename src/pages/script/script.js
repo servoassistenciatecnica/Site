f00c7eae92d3e56db3ded849e6eb5748e664a71a
servoassistenciatecnica <servoassistenciatecnica@gmail.com>
2022-12-23 09:03:59 -0300

    site

---
+++ src/pages/script/script.js
@@ -0,0 +1,33 @@
+const buttom = document.querySelector('button');
+const addloadind = () =>{
+buttom.innerHTML = '<img src="img/iconpage.ico" alt="" srcset="" class="imgsubmit">';
+}
+
+const removeloadind = () =>{
+    buttom.innerHTML = "ENVIAR";
+    }
+
+const inforsubmit = (event) =>{
+   event.preventDefault();
+   addloadind();
+
+    const name = document.querySelector('input[name=nome]').value;
+    const lastname = document.querySelector('input[name=sobrenome]').value;
+    const email = document.querySelector('input[name=email]').value;
+    const text = document.querySelector('textarea[name=textuser]').value;
+
+    fetch("https://api.sheetmonkey.io/form/r2JjtdaRLARrAvynsNB6kf", {
+
+    method:'post',
+    headers:{
+        'accept' : 'application/json',
+        'Content-type' : 'application/json',
+    },
+
+    body: JSON.stringify( {
+        name, lastname, email, text}),
+    }).then(() => removeloadind())
+}
+
+document.querySelector('form').addEventListener('submit', inforsubmit);
+
