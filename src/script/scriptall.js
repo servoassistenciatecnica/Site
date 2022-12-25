69f8595ba5e73e02d3b6ebcb7af44b02d07aaaf3
servoassistenciatecnica <servoassistenciatecnica@gmail.com>
2022-12-25 08:49:12 -0300

    site

--- src/script/scriptall.js
+++ src/script/scriptall.js
@@ -1,7 +1,26 @@
-function loading_p() {
-    document.getElementById('box-load').style.display = "none";
-    document.getElementById('afterheader').style.display = "block";
-    document.getElementById('beforeheader').style.display = "flex";
-    document.getElementById('berofeslider').style.display = "block";
+function clickmenu () {
+        let seta = document.getElementById("arrow_back_ios");
+        let menuline = document.getElementById("lineheadernav");
+    if (seta.style.transform == 'rotate(0deg)') {
+        seta.style.transform = "rotate(272deg)"
+        menuline.style.margin = '15%'
+    } else {
+        seta.style.transform = "rotate(0deg)"
+        menuline.style.margin = '5%'
+    }
+
+    
 }
 
+function loadindpage () {
+    var loadpage = document.getElementById("loadind")
+    var coteinerafterheader = document.getElementById("coteinerafterheader")
+
+    loadpage.style.display = 'none'
+    coteinerafterheader.style.display = 'block'
+}
+
+function apizap () {
+  location.href = 'https://api.whatsapp.com/send?phone=5541996770932&text=ala%20mane%20deu%20certo'
+
+}
